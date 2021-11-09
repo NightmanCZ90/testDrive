36f07715ea92024c966bc276828452f8e915c8c1
NightmanCZ90 <novak.jan90@gmail.com>
2021-11-09 17:58:44 +0100

    feat: Fetch and render movie titles

--- App.tsx
+++ App.tsx
@@ -1,13 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, SafeAreaView, View, FlatList } from 'react-native';
+
+const url = 'https://raw.githubusercontent.com/RyanHemrick/star_wars_movie_app/master/movies.json'
+
+type Movie = {
+  title: string,
+  episode_number: string,
+  main_characters: string[],
+  description: string,
+  poster: string,
+  hero_image: string,
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [movies, setMovies] = React.useState<Movie[]>([])
+
+  useEffect(() => {
+    (async() => {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data.movies)
+      setMovies(data.movies)
+    })()
+  }, [])
+
+  const renderMovie = ({ item }: any) => (
+    <View>
+      <Text>{item.title}</Text>
     </View>
+  );
+
+  return (
+    <SafeAreaView  style={styles.container}>
+      <FlatList
+        data={movies}
+        renderItem={renderMovie}
+        keyExtractor={(item) => item.episode_number}
+      />
+    </SafeAreaView >
   );
 }
 
